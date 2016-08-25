df84bebcdad87b7d47c05c22fc3c69beb47068c3
linshiguan <1754564162@qq.com>
2016-08-25 16:20:57 +0800

    改变字体
    改变字体

---
+++ ChangeFont/UILabel+FontChange/UILabel+FontChange.h
@@ -0,0 +1,19 @@
+//
+//  UILabel+FontChange.h
+//  LiquoriceDoctorProject
+//
+//  Created by HenryCheng on 15/12/7.
+//  Copyright © 2015年 iMac. All rights reserved.
+//
+/*
+ 1.不这样替换，直接在生命周期方法添加也可以实现
+ 2.怎么知道按钮上面title类型为ButtonLabel
+ 3.字体库的添加的Fonts provided by application
+ 
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (FontChange)
+
+@end
